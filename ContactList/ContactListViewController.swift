a9ff34b6e91be33d395637fe0224879293f0b6b7
Leeann Drees <leeann.drees@detroitlabs.com>
2018-09-06 16:58:27 -0400

    navigation controller setup

--- ContactList/ContactListViewController.swift
+++ ContactList/ContactListViewController.swift
@@ -10,14 +10,12 @@ import UIKit
 
 class ContactListViewController: UITableViewController {
 
+    var contactsToShow: [Contact] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+        navigationController?.navigationBar.prefersLargeTitles = true
     }
 
     override func didReceiveMemoryWarning() {
