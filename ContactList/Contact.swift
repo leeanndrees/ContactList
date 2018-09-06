1ddd803b098f3d4ee85ffe8ff75955106c3c88a1
Leeann Drees <leeann.drees@detroitlabs.com>
2018-09-06 16:26:54 -0400

    setting up Contact struct

---
+++ ContactList/Contact.swift
@@ -0,0 +1,17 @@
+//
+//  Contact.swift
+//  ContactList
+//
+//  Created by DetroitLabs on 9/6/18.
+//  Copyright © 2018 DetroitLabs. All rights reserved.
+//
+
+import Foundation
+
+struct Contact {
+    
+    var firstName: String
+    var lastName: String
+    var phoneNumber: String
+    
+}
