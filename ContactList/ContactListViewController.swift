0dda80282cfee392ca910e1651010e98cdbaeffe
Leeann Drees <leeann.drees@detroitlabs.com>
2018-09-09 20:30:20 -0400

    removing comments

--- ContactList/ContactListViewController.swift
+++ ContactList/ContactListViewController.swift
@@ -6,14 +6,6 @@
 //  Copyright © 2018 DetroitLabs. All rights reserved.
 //
 
-/* Build an iOS app that stores and manages a contact list
- Your app should utilize Model-View-Controller architecture
- On the main page, your app should list each contact’s name in a table view
- The user should be able to swipe to delete items
- Your app should have a button which allows the user to add contacts to the list
- When the user clicks on a name in the table view, they should be taken to a detail page that displays the contact’s name and phone number
- BONUS: Give the user the ability to edit contact details */
-
 import UIKit
 
 class ContactListViewController: UITableViewController, AddContactViewControllerDelegate {
@@ -60,15 +52,7 @@ class ContactListViewController: UITableViewController, AddContactViewController
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-
-    // MARK: - Table view data source
-
-//    override func numberOfSections(in tableView: UITableView) -> Int {
-//        // #warning Incomplete implementation, return the number of sections
-//        return 0
-//    }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return contactsToShow.count
@@ -81,53 +65,12 @@ class ContactListViewController: UITableViewController, AddContactViewController
         return cell
     }
     
-//    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-//        performSegue(withIdentifier: "ShowContactDetail", sender: self.contactsToShow[indexPath.row])
-//    }
-    
-//    func saveSelectedContact(selectedCell: Int) -> Contact {
-//        let selectedContact = contactsToShow[selectedCell]
-//        return selectedContact
-//    }
- 
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
             contactsToShow.remove(at: indexPath.row)
             tableView.deleteRows(at: [indexPath], with: .fade)
         }
-//        } else if editingStyle == .insert {
-//             Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-//        }
     }
-
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    
-    // MARK: - Navigation
 
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if segue.identifier == "AddContact" {
